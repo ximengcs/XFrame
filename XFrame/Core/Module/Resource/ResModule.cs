b7b137f330afc9aa97473ed3c60b2e1cc7bd5c72
ximengcs <ximengcs@163.com>
2023-04-11 23:33:30 +0800

    修复问题

--- XFrame/Core/Module/Resource/ResModule.cs
+++ XFrame/Core/Module/Resource/ResModule.cs
@@ -13,7 +13,7 @@ namespace XFrame.Modules.Resource
     /// </summary>
     [BaseModule]
     [RequireModule(typeof(TaskModule))]
-    public class ResModule : SingletonModule<ResModule>
+    public class ResModule : SingletonModule<ResModule>, IResModule
     {
         #region Inner Fields
         private IResourceHelper m_ResHelper;
@@ -24,33 +24,26 @@ namespace XFrame.Modules.Resource
         protected override void OnInit(object data)
         {
             base.OnInit(data);
-            m_PreLoadRes = new Dictionary<string, object>();
+            InnerEnsurePreload();
             if (!string.IsNullOrEmpty(XConfig.DefaultRes))
             {
                 Type type = TypeModule.Inst.GetType(XConfig.DefaultRes);
                 InnerSetHelper(type);
-                m_ResHelper.OnInit(XConfig.ResPath);
             }
         }
 
         protected IResourceHelper InnerSetHelper(Type type)
         {
             m_ResHelper = Activator.CreateInstance(type) as IResourceHelper;
+            m_ResHelper.OnInit(XConfig.ResPath);
             return m_ResHelper;
         }
         #endregion
 
         #region Interface
-        /// <summary>
-        /// 资源预加载(在预加载完成之后，可调用同步方法直接获取资源，在一些不支持同步方法的接口可预加载以便必要时同步加载资源)
-        /// </summary>
-        /// <param name="resPaths">需要加载的资源列表</param>
-        /// <param name="types">资源类型列表</param>
-        /// <returns>加载任务</returns>
         public ITask Preload(string[] resPaths, Type[] types)
         {
-            if (m_PreLoadRes == null)
-                m_PreLoadRes = new Dictionary<string, object>();
+            InnerEnsurePreload();
             if (resPaths.Length != types.Length)
             {
                 Log.Error("XFrame", $"Preload res path is not equal types");
@@ -70,71 +63,42 @@ namespace XFrame.Modules.Resource
             return allTask;
         }
 
-        /// <summary>
-        /// 加载资源(同步)
-        /// </summary>
-        /// <param name="resPath">资源路径</param>
-        /// <param name="type">资源类型</param>
-        /// <returns>加载到的资源</returns>
         public object Load(string resPath, Type type)
         {
+            InnerEnsurePreload();
             if (m_PreLoadRes.TryGetValue(resPath, out object asset))
                 return asset;
             else
                 return m_ResHelper.Load(resPath, type);
         }
 
-        /// <summary>
-        /// 加载资源(同步)
-        /// </summary>
-        /// <typeparam name="T">资源类型</typeparam>
-        /// <param name="resPath">资源路径</param>
-        /// <returns>加载到的资源</returns>
         public T Load<T>(string resPath)
         {
+            InnerEnsurePreload();
             if (m_PreLoadRes.TryGetValue(resPath, out object asset))
                 return (T)asset;
             else
                 return m_ResHelper.Load<T>(resPath);
         }
 
-        /// <summary>
-        /// 加载资源(异步)
-        /// </summary>
-        /// <param name="resPath">资源路径</param>
-        /// <param name="type">资源类型</param>
-        /// <returns>加载任务</returns>
         public ResLoadTask LoadAsync(string resPath, Type type)
         {
             return m_ResHelper.LoadAsync(resPath, type);
         }
 
-        /// <summary>
-        /// 加载资源(异步)
-        /// </summary>
-        /// <typeparam name="T">资源类型</typeparam>
-        /// <param name="resPath">资源路径</param>
-        /// <returns>加载任务</returns>
         public ResLoadTask<T> LoadAsync<T>(string resPath)
         {
             return m_ResHelper.LoadAsync<T>(resPath);
         }
 
-        /// <summary>
-        /// 卸载资源
-        /// </summary>
-        /// <param name="target">要卸载的目标</param>
         public void Unload(object target)
         {
             m_ResHelper.Unload(target);
         }
 
-        /// <summary>
-        /// 卸载预加载的资源
-        /// </summary>
-        /// <param name="resPath">资源路径</param>
         public void UnloadPre(string resPath)
         {
+            InnerEnsurePreload();
             if (m_PreLoadRes.TryGetValue(resPath, out object asset))
             {
                 Unload(asset);
@@ -142,23 +106,24 @@ namespace XFrame.Modules.Resource
             }
         }
 
-        /// <summary>
-        /// 卸载所有已经加载的资源
-        /// </summary>
         public void UnloadAll()
         {
             m_ResHelper.UnloadAll();
         }
 
-        /// <summary>
-        /// 卸载所有预加载的资源
-        /// </summary>
         public void UnloadAllPre()
         {
+            InnerEnsurePreload();
             foreach (object asset in m_PreLoadRes.Values)
                 m_ResHelper.Unload(asset);
             m_PreLoadRes.Clear();
         }
         #endregion
+
+        private void InnerEnsurePreload()
+        {
+            if (m_PreLoadRes == null)
+                m_PreLoadRes = new Dictionary<string, object>();
+        }
     }
 }
