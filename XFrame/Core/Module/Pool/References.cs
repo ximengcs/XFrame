eb69abb8abb5d46fd4f1458e8e26c6adb162f3b7
ximengcs <ximengcs@163.com>
2023-06-03 14:06:33 +0800

    添加接口

--- XFrame/Core/Module/Pool/References.cs
+++ XFrame/Core/Module/Pool/References.cs
@@ -1,8 +1,16 @@
 ﻿
+using System;
+
 namespace XFrame.Modules.Pools
 {
     public static class References
     {
+        public static IPoolObject Require(Type type)
+        {
+            IPool pool = PoolModule.Inst.GetOrNew(type);
+            return pool.Require();
+        }
+
         public static T Require<T>() where T : IPoolObject
         {
             IPool<T> pool = PoolModule.Inst.GetOrNew<T>();
