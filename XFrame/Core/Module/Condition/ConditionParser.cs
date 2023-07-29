35018526f725c0d77c95a039314a5dac19505da5
ximeng <ximengcs@163.com>
2023-07-29 12:53:26 +0800

    add parser

---
+++ XFrame/Core/Module/Condition/ConditionParser.cs
@@ -0,0 +1,48 @@
+﻿
+using XFrame.Core;
+using XFrame.Modules.Pools;
+
+namespace XFrame.Modules.Conditions
+{
+    public class ConditionParser : IParser<ConditionData>
+    {
+        private ConditionData m_Value;
+
+        public ConditionData Value => m_Value;
+
+        public int PoolKey => default;
+
+        object IParser.Value => m_Value;
+
+        public ConditionData Parse(string pattern)
+        {
+            m_Value = new ConditionData(pattern);
+            return m_Value;
+        }
+
+        void IPoolObject.OnCreate()
+        {
+
+        }
+
+        void IPoolObject.OnDelete()
+        {
+
+        }
+
+        void IPoolObject.OnRelease()
+        {
+            m_Value = default;
+        }
+
+        void IPoolObject.OnRequest()
+        {
+
+        }
+
+        object IParser.Parse(string pattern)
+        {
+            return Parse(pattern);
+        }
+    }
+}
