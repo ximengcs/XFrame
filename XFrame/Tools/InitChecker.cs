0f457a4727adce00b8960eb145ec1e85b3acbee8
ximengcs <ximengcs@163.com>
2023-12-13 16:24:26 +0800

    初始化检查器

---
+++ XFrame/Tools/InitChecker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using XFrame.Modules.Pools;
+
+namespace XFrame.Tools
+{
+    public class InitChecker : PoolObjectBase, IPoolObject
+    {
+        private bool m_Inited;
+        private Action m_Callback;
+
+        private InitChecker() { }
+
+        public void Trigger()
+        {
+            if (m_Inited) return;
+            m_Inited = true;
+            m_Callback?.Invoke();
+            m_Callback = null;
+        }
+
+        public void Listen(Action callback)
+        {
+            if (m_Inited)
+            {
+                callback?.Invoke();
+            }
+            else
+            {
+                m_Callback = callback;
+            }
+        }
+
+        protected internal override void OnReleaseFromPool()
+        {
+            base.OnReleaseFromPool();
+            m_Inited = false;
+            m_Callback = null;
+        }
+
+        protected internal override void OnRequestFromPool()
+        {
+            base.OnRequestFromPool();
+            m_Inited = false;
+            m_Callback = null;
+        }
+
+        public static InitChecker Create()
+        {
+            InitChecker inst = References.Require<InitChecker>();
+            return inst;
+        }
+    }
+}
