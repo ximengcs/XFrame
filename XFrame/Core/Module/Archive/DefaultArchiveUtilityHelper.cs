28102c48556fda45cd39850ec8f272a4db192628
ximengcs <simon.xi@buttontech.com>
2024-02-02 15:22:36 +0800

    update

---
+++ XFrame/Core/Module/Archive/DefaultArchiveUtilityHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+
+namespace XFrame.Modules.Archives
+{
+    internal class DefaultArchiveUtilityHelper : IArchiveUtilityHelper
+    {
+        public byte[] ReadAllBytes(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+
+        public void WriteAllBytes(string path, byte[] buffer)
+        {
+            File.WriteAllBytes(path, buffer);
+        }
+
+        public string ReadAllText(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public void WriteAllText(string path, string text)
+        {
+            File.WriteAllText(path, text);
+        }
+    }
+}
---
+++ XFrame/Core/Module/Archive/IArchiveUtilityHelper.cs
@@ -0,0 +1,11 @@
+﻿
+namespace XFrame.Modules.Archives
+{
+    public interface IArchiveUtilityHelper
+    {
+        byte[] ReadAllBytes(string path);
+        void WriteAllBytes(string path, byte[] data);
+        string ReadAllText(string path);
+        void WriteAllText(string path, string data);
+    }
+}
