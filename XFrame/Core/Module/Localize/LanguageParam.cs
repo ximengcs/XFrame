a0dfb6e592d38bf4906a3c956818a7a2b5bf7bdf
ximengcs <simon.xi@buttontech.com>
2024-02-07 10:33:56 +0800

    UP

--- XFrame/Core/Module/Localize/LanguageParam.cs
+++ XFrame/Core/Module/Localize/LanguageParam.cs
@@ -5,11 +5,27 @@ namespace XFrame.Modules.Local
     {
         public int Id;
         public object[] Params;
+
+        public static LanguageParam Create(int id, params object[] args)
+        {
+            LanguageParam param = new LanguageParam();
+            param.Id = id;
+            param.Params = args;
+            return param;
+        }
     }
 
     public struct LanguageIdParam
     {
         public int Id;
         public int[] Params;
+
+        public static LanguageIdParam Create(int id, params int[] args)
+        {
+            LanguageIdParam param = new LanguageIdParam();
+            param.Id = id;
+            param.Params = args;
+            return param;
+        }
     }
 }
