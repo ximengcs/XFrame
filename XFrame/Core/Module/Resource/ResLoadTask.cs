a9d40d63f5ee2d747b6eaca7f1e38a2b7b13cd3b
ximengcs <ximengcs@163.com>
2024-04-25 23:40:28 +0800

    update

--- XFrame/Core/Module/Resource/ResLoadTask.cs
+++ XFrame/Core/Module/Resource/ResLoadTask.cs
@@ -4,15 +4,33 @@ namespace XFrame.Modules.Resource
 {
     public class ResLoadTask : XProTask
     {
+        private IResHandler m_Handler;
+
         public ResLoadTask(IResHandler handler, XTaskCancelToken cancelToken = null) : base(handler, cancelToken)
         {
+            m_Handler = handler;
+        }
+
+        protected override void InnerStart()
+        {
+            m_Handler.Start();
+            base.InnerStart();
         }
     }
 
     public class ResLoadTask<T> : XProTask<T>
     {
+        private IResHandler m_Handler;
+
         public ResLoadTask(IResHandler handler, XTaskCancelToken cancelToken = null) : base(handler, cancelToken)
         {
+            m_Handler = handler;
+        }
+
+        protected override void InnerStart()
+        {
+            m_Handler.Start();
+            base.InnerStart();
         }
 
         /// <summary>
