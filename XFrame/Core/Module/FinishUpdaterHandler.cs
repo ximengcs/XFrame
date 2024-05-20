896a67ac211527066fae64844c8dfec75c77a3e8
ximengcs <ximengcs@163.com>
2024-05-20 13:48:50 +0800

    update

---
+++ XFrame/Core/Module/FinishUpdaterHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace XFrame.Core.Module
+{
+    /// <summary>
+    /// 更新处理器
+    /// </summary>
+    public class FinishUpdateHandler : IModuleHandler
+    {
+        /// <inheritdoc/>
+        public Type Target => typeof(IFinishUpdater);
+
+        /// <inheritdoc/>
+        public void Handle(IModule module, object data)
+        {
+            IFinishUpdater updater = module as IFinishUpdater;
+            updater.OnUpdate((double)data);
+        }
+    }
+}
