0d3b5404606c23fdb6be586f37d6b1c27fc5613c
ximengcs <simon.xi@buttontech.com>
2024-05-23 17:03:40 +0800

    update

--- XFrame/Core/Module/Thread/FiberExtension.cs
+++ XFrame/Core/Module/Thread/FiberExtension.cs
@@ -1,22 +1,25 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 
 namespace XFrame.Core.Threads
 {
     public static class FiberExtension
     {
-        public static void StartThread(this Fiber param)
+        public static void StartThread(this Fiber param, int waitMillSecondTime = 0)
         {
             Thread thread = new Thread((p) =>
             {
-                Fiber fiber = p as Fiber;
+                ValueTuple<Fiber, int> data = (ValueTuple<Fiber, int>)p;
+                int time = data.Item2;
+                Fiber fiber = data.Item1;
                 fiber.Use();
                 while (true)
                 {
-                    fiber.Update(0);
-                    Thread.Sleep(0);
+                    fiber.Update(time);
+                    Thread.Sleep(time);
                 }
             });
-            thread.Start(param);
+            thread.Start(ValueTuple.Create(param, waitMillSecondTime));
             param.SetThread(thread);
         }
     }
