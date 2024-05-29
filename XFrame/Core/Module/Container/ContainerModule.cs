34fd55694d64cdd895e5da639d5445b51fee60ff
ximengcs <ximengcs@163.com>
2024-05-29 21:59:45 +0800

    update

--- XFrame/Core/Module/Container/ContainerModule.cs
+++ XFrame/Core/Module/Container/ContainerModule.cs
@@ -23,7 +23,6 @@ namespace XFrame.Modules.Containers
 
         public void GetAll(List<IContainer> list)
         {
-            Log.Debug($"{GetHashCode()} {Id} GetAll");
             foreach (var entry in m_Containers)
                 list.Add(entry.Value);
         }
@@ -54,7 +53,6 @@ namespace XFrame.Modules.Containers
 
         private IContainer InnerNew(Type type, int id, bool updateTrusteeship, IContainer master, OnDataProviderReady onReady)
         {
-            Log.Debug($"{GetHashCode()} {Id} InnerNew");
             IContainer container = Domain.TypeModule.CreateInstance(type) as IContainer;
             m_Containers.Add(id, container);
             m_ContainersList.Add(container);
@@ -127,7 +125,6 @@ namespace XFrame.Modules.Containers
                 m_UpdateList.Remove(container.Id);
             if (m_ContainersList.Contains(container))
             {
-                Log.Debug($"{GetHashCode()} {Id} Remove");
                 m_ContainersList.Remove(container);
             }
             if (m_Containers.ContainsKey(container.Id))
